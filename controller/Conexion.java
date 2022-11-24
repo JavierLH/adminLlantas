ba6138a178196623d5be96bf3e404a67ff59745f
JavierLH <abislopeznande@gmail.com>
2022-11-24 10:22:14 -0600

    creacion de la conexion de base de datos.

---
+++ controller/Conexion.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static String url = "jdbc:mysql://127.0.0.1:3306/ejemplo80640";
+    private static String DriverName = "com.mysql.jdbc.Driver";
+    private static String username = "root";
+    private static String password = "123456789";
+    private static Connection connection = null;
+
+    public static Connection getConnection(){
+        try {
+            Class.forName(DriverName);
+            connection = (Connection) DriverManager.getConnection(url, username, password);
+            System.out.println("listo conectado");
+        } catch (SQLException e){
+            System.out.println(e);
+        } catch (ClassNotFoundException e) {
+            System.out.println("no encontro el driver");
+        }
+        return connection;
+    }
+    
+}
